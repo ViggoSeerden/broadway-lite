2bb7c9f8cdd0e04f4cf4d58339680867e02af8c2
ViggoSeerden <491216@student.fontys.nl>
2024-01-19 14:24:53 +0100

    sw fix 3

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-restricted-globals */
 const offlinePage = 'offline.html';
-// @ts-ignore
+/* eslint-disable */
 const selfManifest = self.__WB_MANIFEST;
+/* eslint-enable */
 const cacheName = "broadwayCache";
 //Just take into account that the "files" below are request-url's and not filenames perse. So for your root of your website yous should include "./" and if you use my site (or another plain HTML-site) also "index.html". If you use a server-side language and have friendly url's that could be something like "news/this-is-a-newsarticle/".
 const appFiles = [
